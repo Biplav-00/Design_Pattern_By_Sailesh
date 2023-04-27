4473287793c5bc7c48e2353cf8b1511f76548fb9
Biplav-00 <biplav.ghimire@truenary.com>
2023-04-27 15:43:47 +0545

    new program file is added

---
+++ AbstractDesignPatternExample/saileshDemo.cs
@@ -0,0 +1,9 @@
+namespace AbstractDesignPatternExample;
+
+public class saileshDemo
+{
+    public void addMe()
+    {
+        Console.WriteLine(" I am new added file but no-one used me .....");
+    }
+}
