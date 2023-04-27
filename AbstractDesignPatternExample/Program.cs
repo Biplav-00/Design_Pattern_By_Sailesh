0bbdcd241aee3a27776b503ac6f8f3cabd5d9368
Biplav-00 <biplav.ghimire@truenary.com>
2023-04-27 15:16:09 +0545

    sailesh main fucntion ma comment add gareko xu hai

---
+++ AbstractDesignPatternExample/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace AbstractDesignPatternExample;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        IAbsractFactory abstractobject = FactoryProducer.ChooseFactory("shape");
+        IShape shape1 = new Circle();
+        shape1.Size();
+        
+        //do whatever you want
+    }
+}
+
