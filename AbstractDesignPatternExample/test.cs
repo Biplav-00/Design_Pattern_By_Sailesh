4a64029f71139099070725cee57224ba7b1e1cc7
Biplav-00 <biplav.ghimire@truenary.com>
2023-04-27 15:51:04 +0545

    new test file is added in test branch

---
+++ AbstractDesignPatternExample/test.cs
@@ -0,0 +1,11 @@
+namespace AbstractDesignPatternExample;
+
+public class test
+{
+    public string finalValue
+    {
+        //this file is added as new file 
+        //Test carefully
+        get { return "okey"; }
+    }
+}
